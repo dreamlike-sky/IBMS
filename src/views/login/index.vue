fa58207ab63e7cb8b721da5be29b8389c42ea390
tainde <1245703952@qq.com>
2022-03-14 19:02:09 +0800

    用户登录-页面布局添加样式

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,5 +1,25 @@
 <template>
-  <div class="login-container">登录页面</div>
+  <div class="login-container">
+    <div class="login-form-wrap">
+      <div class="login-head">
+        <div class="logo"></div>
+      </div>
+      <el-form class="login-form" ref="form" :model="user">
+        <el-form-item>
+          <el-input v-model="user.mobile" placeholder="请输入手机号"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input v-model="user.code" placeholder="请输入验证码"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-checkbox v-model="checked">我已阅读并同意用户协议和隐私条款</el-checkbox>
+        </el-form-item>
+        <el-form-item>
+          <el-button class="login-btn" type="primary" @click="onSubmit">登录</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -8,16 +28,58 @@ export default {
   Components: {},
   props: {},
   data () {
-    return {}
+    return {
+      user: {
+        mobile: '',
+        code: ''
+      },
+      checked: false
+    }
   },
   computed: {},
   watch: {},
   created () {},
   mounted () {},
-  methods: {}
+  methods: {
+    onSubmit () {
+      console.log('submit!')
+    }
+  }
 }
 </script>
 
 <style scoped lang="less">
-
+.login-container {
+  position: fixed;
+  left: 0;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background: url("./login_bg.jpg") no-repeat;
+  background-size: cover;
+  .login-form-wrap{
+    background-color: #fff;
+    .login-head {
+      padding: 15px 0;
+      height: 52px;
+      .logo {
+        margin: auto;
+        width: 167px;
+        height: 100%;
+        background: url("./logo_index.png") no-repeat;
+      }
+    }
+    .login-form {
+      padding: 0 50px;
+      min-width: 300px;
+      .login-btn {
+        width: 100%;
+      }
+    }
+  }
+}
 </style>
